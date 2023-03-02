1bce79c3f4c90871a1cebfd0de60ae391d4016a0
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-03-02 16:43:40 -0600

    cleaned up html by removing hardcoded modals and using js to make them for us

--- main.js
+++ main.js
@@ -82,31 +82,87 @@ function organizeCoffees(filteredCoffees){
 
 function addCoffee(e) {
     e.preventDefault()
-    let coffeeId = coffees.length + 1
-    let coffeeName = coffeeAdd.value
-    let coffeeRoast = roastAdd.value
-    let newCoffee = {id: coffeeId, name: coffeeName, roast: coffeeRoast}
-    coffees.push(newCoffee)
-    organizeCoffees(coffees)
+    let coffeeId = coffees.length + 1;
+    let coffeeName = coffeeAdd.value;
+    let coffeeRoast = roastAdd.value;
+    let newCoffee = {id: coffeeId, name: coffeeName, roast: coffeeRoast};
+    coffees.push(newCoffee);
+    organizeCoffees(coffees);
+    createModals(coffees);
+}
+
+function createModals(coffees){
+    let html = '';
+    coffees.forEach(function(coffee) {
+        html += '<div class="modal fade" id="modalBox' + coffee.id + '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">'
+        html += '<div class="modal-dialog">';
+        html += '<div class="modal-header">';
+        html += '<h5 class="modal-title" id="exampleModalLabel">' + coffee.name + '</h5>';
+        html += '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>';
+        html += '</div>';
+        html += '<div class="modal-body text-center">';
+        if(coffee.id > 14){
+            html += 'It appears you have input a coffee roast that we don\'t have listed. We would love to accomadate that!\n' +
+                '                    Please contact us at; <a href="https://www.gmail.com">gandjcoffee@googlemail.com</a>'
+        } else {
+            html += coffee.description;
+        }
+        html += '</div>'
+        html += '<div class="modal-footer">';
+        html += '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>';
+        html += '</div>';
+        html += '</div>';
+        html += '</div>';
+        html += '</div>';
+    });
+    coffeeModals.innerHTML = html;
 }
 
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 let coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light', description: 'The Light City Coffee is a roast also called a New England roast by some coffee connoisseurs.\n' +
+            '                    This is the first stage in which the coffee becomes drinkable during the roasting process. A true way to\n' +
+            '                    differentiate between the quality of different beans!'},
+    {id: 2, name: 'Half City', roast: 'light', description: 'The Half City Coffee is roasted similarly to full city coffee, left only slightly longer during the process.\n' +
+            '                    Only a true coffee fanatic will be able to discern the differences between the two, however the notes are subtle\n' +
+            '                    yet very altering.'},
+    {id: 3, name: 'Cinnamon', roast: 'light', description: 'Don\'t let our Cinnamon roasted coffee fool you, it has nothing to do with the taste! This roast is\n' +
+            '                    referred to a Cinnamon roast due to it\'s color when discharged from the roasting process. This roast\n' +
+            '                    is for drinkers who aren\'t quite ready for a Medium roast, but need something with a bit more kick than\n' +
+            '                    our lighter Light and Half City roasts!'},
+    {id: 4, name: 'City', roast: 'medium', description: 'Our City roast most famous roast of coffee! During our roasting process, our famous City roast is the\n' +
+            '                    first step in our line of Medium roast coffees. This is where the flavor truly begins to peak without\n' +
+            '                    becoming too bitter. A great introduction into the world of flavorful coffee!'},
+    {id: 5, name: 'American', roast: 'medium', description: 'Our American Roast is the point where our ethically sourced beans have achieved their rich and medium\n' +
+            '                    brown color without any of it\'s natural oils appearing on the surface of the liquid. This roast produces\n' +
+            '                    a very flavorful yet complex cup of coffee with a signature bold and snappy flavor to it.'},
+    {id: 6, name: 'Breakfast', roast: 'medium', description: 'Every Breakfast roast coffee is different in it\'s own way, however ours is an excellent middle of the\n' +
+            '                    road choice. Experts have described it as mild, balanced, and straightforward. A go-to choice for your\n' +
+            '                    first morning cup of coffee!'},
+    {id: 7, name: 'High', roast: 'dark', description: 'Our High Roast coffee is, as the name implies, roasted at a slightly higher temperature than our normal medium\n' +
+            '                    roasts. It\'s much similar to the Breakfast roast, with just a little bit more of a sharp flavor profile. Good\n' +
+            '                    for those who truly want an energetic start to their morning!'},
+    {id: 8, name: 'Continental', roast: 'dark', description: 'Want a little darker and more of a full-body flavored roast than our High roast? Look no further than our\n' +
+            '                    Continental roasted coffee! Our Continental roast is loaded with flavor notes of deep cocoa, red apple,\n' +
+            '                    and caramel!'},
+    {id: 9, name: 'New Orleans', roast: 'dark', description: 'Our signature New Orleans roast coffee brews a rich, dark roast with a robust flavor profile. A true taste\n' +
+            '                    of New Orlean\'s energetic and artful culture!'},
+    {id: 10, name: 'European', roast: 'dark', description: 'Just got back from a trip to Europe and want a package of coffee that reminds you of your experience?\n' +
+            '                    Well, our European coffee fits your desire perfectly! With a deep and full flavored dark roast with smoky\n' +
+            '                    overtones, yet delightfully smooth, you\'ll feel like you\'re in a Paris Cafe inside your own kitchen!'},
+    {id: 11, name: 'Espresso', roast: 'dark', description: 'Ah, a true classic. The Espresso. One of our darker and richer roasts to date. Want a rich, intense,\n' +
+            '                    yet never-bitter coffee? Our signature Espresso Roast is the choice for you. Blended and roasted\n' +
+            '                    to perfection to bring our our high profile flavors, a true coffee nut must-have!'},
+    {id: 12, name: 'Viennese', roast: 'dark', description: 'Our Viennese roast coffee is a medium-dark roast that has the richness of a dark roast, without the\n' +
+            '                    carbon-smoky flavor. Our beans are roasted to a deep dark reddish brown and only slightly oily to perfect\n' +
+            '                    the flavor profile.'},
+    {id: 13, name: 'Italian', roast: 'dark', description: 'Our second-darkest roast available! Our beans are roasted to an exceptionally oily and dark color.\n' +
+            '                    This is not for the faint of heart, packed with a heavy kick and intense flavor, this is only for\n' +
+            '                    our most serious of coffee connoisseurs!'},
+    {id: 14, name: 'French', roast: 'dark', description: 'The heaviest, and darkest roasted coffee available from our selection. We roast these beans for slightly\n' +
+            '                    longer than is standard. Though most frown upon the practice, we have perfected the artform to produce\n' +
+            '                    the most packed beans possible! Intense, intense, intense!'},
 ];
 
 const coffeeCards = document.querySelector('#coffees');
@@ -117,9 +173,11 @@ const coffeeAdd = document.querySelector('#coffee-add-bar');
 const roastAdd = document.querySelector('#roast-selection-add')
 const submitButtonAdd = document.querySelector('#submit-add')
 const clearButton = document.querySelector('#clear-coffee')
+const coffeeModals = document.querySelector('#coffeeModals')
 
 retrievePreviouslyAddedCoffees(coffees)
 organizeCoffees(coffees);
+createModals(coffees);
 
 
 coffeeSearch.addEventListener('keyup', searchByName)
